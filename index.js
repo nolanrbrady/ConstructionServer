79bdefe3e96afd1b62379d612ad2592585f47c83
Nolan Brady <nolanrbrady@gmail.com>
2021-10-27 17:24:31 -0600

    Built routes to tie into on frontend

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+    console.log("Req", req);
+    console.log("Res", res);
+
+    // const { route } = res.data;
+    const route = '/lod';
+
+    switch(route.path) {
+        case "/lod":
+            console.log("Changing LOD");
+            this.changeLOD(res);
+        case "/panel":
+            console.log("Changing panel");
+            this.changePanel(res);
+
+        default:
+            res.send("Error. Please check route");
+    };
+});
+
+const changeLOD = (res) => {
+    console.log("Change LOD Function Firing");
+    res.send("LOD Changed");
+};
+
+const changePanel = (res) => {
+    console.log("Change Panel Function is Firing");
+    res.send("Panel Changed");
+};
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
