afde53f737bd43acd6ac33b0fce387ca299ccef1
Nolan Brady <nolanrbrady@gmail.com>
2021-11-02 11:54:01 -0600

    parsing body of incoming data successfully

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const upload = multer();
 const app = express()
 const port = 3001
 
+// Needed to avoid CORS errors due to split stack
 app.use(cors());
 
-app.put('/', (req, res) => {
-    // console.log("Req", req);
-    // console.log("Res", res);
+// Needed to parse the incoming data from front end
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.put('/', upload.array(), (req, res, next) => {
+   try {
+    console.log("Req body", req.body);
     const route = req.url;
 
     switch(route) {
         case "/":
             changeLOD(res);
+            break;
         case "/panel":
             changePanel(res);
+            break;
 
         default:
             res.send("Error. Please check route");
     };
+   } catch(e){
+    next(e);
+   }
 });
 
 const changeLOD = (res) => {
