4f70f624c0b0835c2b16e9c64182b06ec019b67d
Nolan Brady <nolanrbrady@gmail.com>
2021-11-07 14:16:36 -0700

    naming conflict

--- models/construction.model.js
+++ models/construction.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
-    const Construction = sequelize.define("remote-control", {
+    const Construction = sequelize.define("remote-controls", {
       lod: {
         type: Sequelize.INTEGER,
         defaultValue: 200
