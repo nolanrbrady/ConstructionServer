00c6cdbc91d1b6a9c5ce657027bd8eedef000adc
Nolan Brady <nolanrbrady@gmail.com>
2023-07-25 16:52:37 -0600

    Fixing raw sql error

--- index.js
+++ index.js
@@ -101,21 +101,6 @@ app.get('/config-change-log', async(req, res, next) => {
     }
 })
 
-app.get('/session-tracking-data', async (req, res, next) => {
-    try {
-        const session_data = await SessionData.findAll({
-		attributes: ['eyeTracking'],
-		order: [['createdAt', 'DESC']],
-		limit: 10,
-	});
-        if(!session_data) res.send("No session data available");
-        else res.send(session_data.slice(-5);
-    } catch (err) {
-        next(err);
-        res.send(`There was an error getting session tracking data: ${err}`);
-    }
-});
-
 app.get('/all-session-tracking-data', async (req, res, next) => {
     try {
         const session_data = await SessionData.findAll();
